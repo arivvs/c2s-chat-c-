3223432c5e74e401d788ae6b715bb7caa4377aa0
arros <arros@RIVV>
2026-01-20 12:24:32 +0200

    Добавьте файлы проекта.

---
+++ yomayo client/client.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace yomayo_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Client";
+
+            Console.Write("Nickname: ");
+            string userName = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"Ur nick {userName}. Type 'exit' to quit.");
+
+            while (true)
+            {
+                try
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write($"{userName}: ");
+                    string message = Console.ReadLine();
+                    Console.ResetColor();
+
+                    
+                    if (string.IsNullOrEmpty(message) || message.ToLower() == "exit")
+                    {
+                        Console.WriteLine("adios amigo");
+                        break;
+                    }
+
+                    using (TcpClient client = new TcpClient("127.0.0.1", 8888))
+                    {
+                        NetworkStream stream = client.GetStream();
+
+                        string fullMessage = $"{userName}: {message}";
+                        byte[] data = Encoding.UTF8.GetBytes(fullMessage);
+                        stream.Write(data, 0, data.Length);
+
+                        byte[] responseBuffer = new byte[1024];
+                        int bytesRead = stream.Read(responseBuffer, 0, responseBuffer.Length);
+                        string response = Encoding.UTF8.GetString(responseBuffer, 0, bytesRead);
+
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine(response);
+                        Console.ResetColor();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("error: " + ex.Message);
+                    Console.WriteLine("Press any key to try again...");
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
---
+++ yomayo serv/serv.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace yomayo_serv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "server";
+            string myName = "admin"; // You can change the server's name here
+
+            TcpListener server = new TcpListener(IPAddress.Any, 8888);
+            server.Start();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("All started");
+            Console.ResetColor();
+
+            while (true)
+            {
+                using (TcpClient client = server.AcceptTcpClient())
+                {
+                    string remoteIp = client.Client.RemoteEndPoint.ToString();
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    Console.WriteLine($"[LOG]: Connected ip {remoteIp}");
+                    Console.ResetColor();
+
+                    NetworkStream stream = client.GetStream();
+
+                    byte[] buffer = new byte[1024];
+                    int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                    string incomingData = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine($"\n[{DateTime.Now:HH:mm}] {incomingData}");
+                    Console.ResetColor();
+
+                    Console.Write("Want to ans? (y/n): ");
+
+                    
+                    while (true)
+                    {
+                        ConsoleKey key = Console.ReadKey(true).Key;
+
+                        if (key == ConsoleKey.Y)
+                        {
+                            Console.WriteLine("y");
+                            Console.ForegroundColor = ConsoleColor.Magenta;
+                            Console.Write($"{myName}: ");
+                            string response = Console.ReadLine();
+
+                            string fullResponse = $"{myName}: {response}";
+                            byte[] responseData = Encoding.UTF8.GetBytes(fullResponse);
+                            stream.Write(responseData, 0, responseData.Length);
+                            Console.ResetColor();
+                            break;
+                        }
+                        else if (key == ConsoleKey.N)
+                        {
+                            Console.WriteLine("n");
+                            Console.WriteLine("\nYou ignored");
+                            byte[] silent = Encoding.UTF8.GetBytes("You were ignored");
+                            stream.Write(silent, 0, silent.Length);
+                            break;
+                        }
+                        
+                    }
+                }
+            }
+        }
+    }
+}
